8a597dd1adc5b09ef1167a12abada1863cb5d069
Madhiha13 <madhihakhanamsk13@gmail.com>
2024-06-19 23:03:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FacilityEmissionsHome from './Components/FacilityEmissionsHome' 
+
+
+const App = () => {
+  return (
+    <div>
+      <FacilityEmissionsHome />
+     
+    </div>
+  )
+}
+
+export default App
